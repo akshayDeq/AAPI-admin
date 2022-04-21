d4bcde269a09c30b239ac04dfa92459b7409469d
akshayDeq <akanojiya@deqode.com>
2022-04-21 16:21:30 +0530

    Create : Export file for admin module

---
+++ src/admins/index.js
@@ -0,0 +1,16 @@
+// import AdminCreate from "./AdminCreate";
+import AdminEdit from "./AdminEdit";
+import AdminList from "./AdminList";
+import AdminCreate from "./AdminCreate";
+import AdminShow from "./AdminShow.js";
+import AdminPanelSettingsIcon from "@mui/icons-material/AdminPanelSettings";
+
+const Icon = () => <AdminPanelSettingsIcon style={{ color: "white" }} />;
+
+export default {
+  list: AdminList,
+  create: AdminCreate,
+  edit: AdminEdit,
+  show: AdminShow,
+  icon: Icon,
+};
