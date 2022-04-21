df05c85973e144fdf4bf961fafb555930da6b87e
akshayDeq <akanojiya@deqode.com>
2022-04-21 15:36:07 +0530

    Create : Global stylesheet

---
+++ src/Style/globalStyles.js
@@ -0,0 +1,23 @@
+import { makeStyles } from "@material-ui/styles";
+
+export const globalStyles = makeStyles({
+  button: {
+    color: "#223269",
+  },
+
+  tableMargin: {
+    marginTop: "25px",
+  },
+
+  headerCell: {
+    fontWeight: "bold",
+    color: "#223269",
+    backgroundColor: "#E5E5E5",
+    fontSize: "15px",
+  },
+
+  label: {
+    fontSize: "18px",
+    color: "black",
+  },
+});
