294f429773ceef30cf458a7dd93d6b7233d2d5ef
akshayDeq <akanojiya@deqode.com>
2022-05-16 16:13:53 +0530

    Update : Include role based features for super admin

--- src/admins/AdminList.js
+++ src/admins/AdminList.js
@@ -6,33 +6,59 @@ import {
   TextInput,
   ShowButton,
   DateField,
+  SelectInput,
 } from "react-admin";
 import { globalStyles } from "../Style/globalStyles";
+import { useState, useEffect } from "react";
 
 const AdminFilter = [
-  <TextInput label="Id" source="id" />,
-  <TextInput label="Email" source="email" />,
-  <TextInput label="Status" source="status" defaultValue="active" />,
+  <TextInput label="Search" source="q" alwaysOn />,
+  <TextInput autoComplete="off" label="ID" source="id" />,
+  <TextInput autoComplete="off" label="Email" source="email" />,
+  <SelectInput
+    label="Status"
+    source="status"
+    choices={[{ name: "Active" }, { name: "Inactive" }]}
+    optionValue="name"
+  />,
 ];
 
 const AdminList = (props) => {
   const classes = globalStyles();
+  const [userRole] = useState(localStorage.getItem("role"));
+  const [takeBulkActions, setTakeBulkActions] = useState(false);
+
+  useEffect(() => {
+    if (userRole === "super-admin") {
+      setTakeBulkActions(true);
+    } else {
+      setTakeBulkActions(false);
+    }
+  }, [userRole]);
 
   return (
-    <List className={classes.tableMargin} {...props} filters={AdminFilter}>
+    <List
+      className={classes.tableMargin}
+      {...props}
+      filters={AdminFilter}
+      bulkActionButtons={takeBulkActions}
+    >
       <Datagrid
         classes={{
           headerCell: classes.headerCell,
         }}
-        rowClick="show"
       >
-        <TextField label="Id" source="id" />
+        <TextField label="ID" source="id" />
         <TextField label="Email" source="email" />
         <TextField label="Current Sign In" source="current_signin" />
         <TextField label="Sign In Count" source="signin_count" />
         <DateField label="Created On" format="dd/MM/yyyy" source="createdAt" />
         <TextField label="Status" source="status" />
-        <EditButton label="" className={classes.button} basePath="/admin" />
+        {userRole === "super-admin" ? (
+          <EditButton label="" className={classes.button} basePath="/admin" />
+        ) : (
+          <></>
+        )}
         <ShowButton label="" className={classes.button} basePath="/admin" />
       </Datagrid>
     </List>
